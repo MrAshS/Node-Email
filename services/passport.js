af94d2975970c5446d27ffdfe143cbf128e522ff
Ashton Sham <ash@Ashtons-MacBook-Pro.local>
2020-06-23 00:31:42 -0400

    fixing error

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,16 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require("mongoose");
-const keys = require("../config/keys");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
+  User.findById(id).then(user => {
     done(null, user);
   });
 });
@@ -20,17 +20,18 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
-          //We already have record of User with ID
+          // we already have a record with the given profile ID
           done(null, existingUser);
         } else {
+          // we don't have a user record with this ID, make a new record!
           new User({ googleId: profile.id })
             .save()
-            .then((user) => done(null, user));
+            .then(user => done(null, user));
         }
       });
     }
