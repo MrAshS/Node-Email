f32aa105cc9ea8cf256dd3a5feea62ce86165852
Ashton Sham <ash@Ashtons-MacBook-Pro.local>
2020-06-23 00:11:09 -0400

    testing production - auth

--- config/prod.js
+++ config/prod.js
@@ -5,3 +5,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
+
+//mongodb+srv://abcpro:kD0PMYD49wZpWStX@cluster0-37rih.mongodb.net/testpro?retryWrites=true&w=majority
